a3ea5195d85ca4bb022dcb4190a214acee825730
StatistNo1 <80486109+StatistNo1@users.noreply.github.com>
2021-03-17 07:50:43 +0100

    Automating support for humanoid body parts.
    Automating the adding of body parts to humanoid races.
    Automating the adding of meats of humanoid races to protein slag recipes.
    Automating the adding of similar body parts to RecipeDefs of Surgeries. (As long as the Def for the tail have "tail" anywhere in it's defName it will be added for example)
    Deleting of manual patches.

--- 1.2/Source/EvolvedOrgansRedux/EvolvedOrgansRedux/ImmortalityGland.cs
+++ 1.2/Source/EvolvedOrgansRedux/EvolvedOrgansRedux/ImmortalityGland.cs
@@ -1,43 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections;
-using System.Reflection;
-using System.Text;
-using HarmonyLib;
+﻿using HarmonyLib;
 using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Verse;
 
-namespace EvolvedOrgansRedux
-{
-
-    [StaticConstructorOnStartup]
-    public static class Initializer
-    {
-        static Initializer()
-        {
-            Harmony harmony = new Harmony("EvolvedOrgansRedux");
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
-        }
-    }
+namespace EvolvedOrgansRedux {
 
     [HarmonyPatch(typeof(AgeInjuryUtility))]
     [HarmonyPatch("RandomHediffsToGainOnBirthday")]
     [HarmonyPatch(new Type[] { typeof(Pawn), typeof(int) })]
-    static class PreventAgeHediffsIfImmortal_Patch
-    {
-        
+    static class PreventAgeHediffsIfImmortal_Patch {
+
 
         [HarmonyPrefix]
-        public static bool PreventAgeInjuries_Prefix(Pawn pawn, ref IEnumerable<HediffGiver_Birthday> __result)
-        {
-            if (pawn.health.hediffSet.HasHediff(HediffDef.Named("EVOR_Hediff_Abd_NoAge")))
-            {
+        public static bool PreventAgeInjuries_Prefix(Pawn pawn, ref IEnumerable<HediffGiver_Birthday> __result) {
+            if (pawn.health.hediffSet.HasHediff(HediffDef.Named("EVOR_Hediff_Abd_NoAge"))) {
                 Log.Message("no hediff added, pawn is immortal");
                 __result = System.Linq.Enumerable.Empty<HediffGiver_Birthday>();
                 return false;
-            }
-            else
-            {
+            } else {
                 return true;
             }
         }
@@ -46,22 +28,15 @@ namespace EvolvedOrgansRedux
     [HarmonyPatch(typeof(Pawn))]
     [HarmonyPatch("PostApplyDamage")]
     [HarmonyPatch(new Type[] { typeof(DamageInfo), typeof(float) })]
-    static class ApplyPlagueToAttacker_Patch
-    {
-        
+    static class ApplyPlagueToAttacker_Patch {
+
         [HarmonyPostfix]
-        public static void ApplyPlagueToAttacker_Postfix(DamageInfo dinfo, Pawn __instance)
-        {
-            if (__instance.health.hediffSet.HasHediff(HediffDef.Named("EVOR_Hediff_Artifact_Lesions")))
-            {
-                if (!dinfo.Def.isRanged)
-                {
-                    if (((Pawn)dinfo.Instigator).health.hediffSet.HasHediff(HediffDef.Named("EVOR_Hediff_Damage_PurgleRot")))
-                    {
+        public static void ApplyPlagueToAttacker_Postfix(DamageInfo dinfo, Pawn __instance) {
+            if (__instance.health.hediffSet.HasHediff(HediffDef.Named("EVOR_Hediff_Artifact_Lesions"))) {
+                if (!dinfo.Def.isRanged) {
+                    if (((Pawn)dinfo.Instigator).health.hediffSet.HasHediff(HediffDef.Named("EVOR_Hediff_Damage_PurgleRot"))) {
                         ((Pawn)dinfo.Instigator).health.hediffSet.GetFirstHediffOfDef(HediffDef.Named("EVOR_Hediff_Damage_PurgleRot")).Severity += (float)0.05;
-                    }
-                    else
-                    {
+                    } else {
                         ((Pawn)dinfo.Instigator).health.AddHediff(HediffDef.Named("EVOR_Hediff_Damage_PurgleRot"));
                     }
                 }
---
+++ 1.2/Source/EvolvedOrgansRedux/EvolvedOrgansRedux/Initializer.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using System.Reflection;
+
+namespace EvolvedOrgansRedux {
+    [Verse.StaticConstructorOnStartup]
+    public static class Initializer {
+        static Initializer() {
+            Harmony harmony = new Harmony("EvolvedOrgansRedux");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            new AddDefsForEachModdedRace(harmony.Id);
+        }
+    }
+}
